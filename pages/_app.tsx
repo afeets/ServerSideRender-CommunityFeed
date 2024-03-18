b7ee9bfa4c87bf3f06c3b3c461c45d22bdf606f1
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-18 12:25:28 +0700

    Added Header Component and Styled Components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import "@/styles/globals.css";
+import { createGlobalStyle } from 'styled-components';
+import Header from '../components/Header';
+
+const GlobalStyle = createGlobalStyle` 
+  body { 
+    margin: 0; 
+    padding: 0; 
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; 
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale; 
+  } 
+`;
+
+
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
